661c64cc9d09754c5bf7499e5554561443b9c8f8
KRofLife <05410kira@gmail.com>
2021-08-23 16:58:31 +0000

    change

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 has_many :books, dependent: :destroy
 has_many :post_comments, dependent: :destroy
+has_many :favorites, dependent: :destroy
 validates :introduction, length: { maximum: 50 }
   # belongs_to :books
   attachment :profile_image, destroy: false
